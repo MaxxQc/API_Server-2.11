a458f2cea7ddbe4ca419cfb30ebac798292d6d96
SamCloutier07 <minefan07@hotmail.com>
2021-11-22 10:24:48 -0500

    pushed

--- models/new.js
+++ models/new.js
@@ -1,24 +1,22 @@
-module.exports = 
-class New
-{
-    constructor(userId, title, imageGUID, content)
-    {
-        this.Id = 0;
-        this.Title = title !== undefined ? title : "";
-        this.ImageGUID = imageGUID !== undefined ? imageGUID : "";
-        this.Content = content !== undefined ? content : "";
-        this.UserId = userId !== undefined ? userId : 0;
-        this.CreationDate = new Date();
-    }
+module.exports =
+    class New {
+        constructor(userId, title, imageGUID, content, creationDate) {
+            this.Id = 0;
+            this.Title = title !== undefined ? title : "";
+            this.ImageGUID = imageGUID !== undefined ? imageGUID : "";
+            this.Content = content !== undefined ? content : "";
+            this.UserId = userId !== undefined ? userId : 0;
+            this.CreationDate = creationDate !== undefined ? created : 0;
+        }
 
-    static valid(instance) {
-        const Validator = new require('./validator');
-        let validator = new Validator();
-        validator.addField('Id','integer');
-        validator.addField('Title','string');
-        validator.addField('CreationDate', 'datetime');
-        validator.addField('UserId', 'integer');
-        validator.addField('Content','string');
-        return validator.test(instance);
+        static valid(instance) {
+            const Validator = new require('./validator');
+            let validator = new Validator();
+            validator.addField('Id', 'integer');
+            validator.addField('Title', 'string');
+            validator.addField('CreationDate', 'integer');
+            validator.addField('UserId', 'integer');
+            validator.addField('Content', 'string');
+            return validator.test(instance);
+        }
     }
-}
